8baf6241632fa5051e15315f439e9060f866664c
apRodrigues00 <aprodrigues_10@hotmail.com>
2025-03-20 20:04:11 -0300

    feat: criada a página home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: '', component: WelcomeComponent},
+    { path: 'home', component: HomeComponent},
 ];
