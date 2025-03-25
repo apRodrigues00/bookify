312b014606f449c7ecd22faf5d294cbf6fc41d8c
apRodrigues00 <aprodrigues_10@hotmail.com>
2025-03-24 23:58:14 -0300

    chore: added new pages to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BooksComponent } from './pages/books/books.component';
+import { BookDetailsComponent } from './pages/book-details/book-details.component';
+import { ListsComponent } from './pages/lists/lists.component';
 
 export const routes: Routes = [
     { path: '', component: WelcomeComponent},
     { path: 'home', component: HomeComponent},
     { path: 'livros', component: BooksComponent},
+    { path: 'livros/:id', component: BookDetailsComponent},
+    { path: 'listas', component: ListsComponent},
 ];
